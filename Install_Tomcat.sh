d6ef5586d9010994178d14fadcb01abebd51d848
Rod Nwosu <nwosur@gmail.com>
2022-03-02 10:57:27 -0600

    Read comments in the script.

--- Install_Tomcat.sh
+++ Install_Tomcat.sh
@@ -12,17 +12,17 @@ sudo groupadd tomcat # Add a group for Tomcat users.
 sudo usermod -a -G tomcat `whoami` # Assign the user executing this script to the "tomcat" group.
 sudo chown -R root:tomcat /opt/tomcat9 # Change the group ownership of the tomcat directory to "tomcat" group so that only members of this group will have access to this directory.
 sudo chmod -R g+s /opt/tomcat9 # Set special permissions to this directory and sub-directories such that any file created in this directory and deeper inherit the group's permissions.
-sudo ln -s /opt/tomcat9/bin/startup.sh /usr/bin/starttomcat # create a soft link to start Tomcat from anywhere; Type "sudo starttomcat" to start the application. I tried just "starttomcat" but ran into some errors. I haven't figured out yet why I can't run without using sudo; it should be possible.
+sudo chmod g+w /opt/tomcat9/logs/catalina.out # This file needs write permission for whatever reason else you run into an error when trying to use "starttomcat" with using sudo.
+sudo ln -s /opt/tomcat9/bin/startup.sh /usr/bin/starttomcat # create a soft link to start Tomcat from anywhere; Type "starttomcat" to start the application.
 sudo ln -s /opt/tomcat9/bin/shutdown.sh /usr/bin/stoptomcat # create a soft link to stop Tomcat from anywhere; Type "stoptomcat" to stop the application.
 sudo rm /opt/apache-tomcat-9.0.59.tar.gz # Remove the archive from the server.
 
 # The following config. can be added to this script for automatic setup or should be configured manually:
-#       1. File /opt/tomcat9/conf/tomcat-users.xml: Add user and role info (add before </tomcat-users>)
-#       2. File /opt/tomcat9/conf/server.xml (line 69): Change default port 8080 to something else (optional)
-#       3. File /opt/tomcat9/webapps/manager/META-INF/context.xml (line 22-23): Comment out lines that allow local access only to
-#               back-end (optional - only necessary if you want to access back-end from online)
-#       4. Add cron job for Tomcat to start automatically at server startup (optional)
-#       5. Setup access between Maven and Tomcat servers either via ssh or password authentication. Copy the web app from Maven server to
-#               Tomcat server using scp (This is a manual operation; Maven should be able to do this automatically if setup to do so)
-#       6. Start the Tomcat application (Type sudo starttomcat)
+#	1. File /opt/tomcat9/conf/tomcat-users.xml: Add user and role info (add before </tomcat-users>)
+#	2. File /opt/tomcat9/conf/server.xml (line 69): Change default port 8080 to something else (optional)
+#	3. File /opt/tomcat9/webapps/manager/META-INF/context.xml (line 22-23): Comment out lines that allow local access only to back-end (optional - only necessary if you want to access back-end from web)
+#	4. Add cron job for Tomcat to start automatically at server startup (optional)
+#	5. Setup access between Maven and Tomcat servers either via ssh or password authentication. Copy the web app from Maven server to Tomcat server using scp (This is a manual operation; Maven should be 
+#	   able to do this automatically if setup to do so. We did not go over this in class.)
+#	6. Start the Tomcat application (Type starttomcat)
 
